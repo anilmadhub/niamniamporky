7aefa2a23573829ff32e204c7d6075d0c60a44f4
Oumar <oumarkhoda@gmail.com>
2020-06-04 01:27:02 +0400

    fixed responsiveness

--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,12 @@ export default function Home() {
   return (
     <Main>
       <div className="parallax-home">
-        <h3 className="has-text-white-bis is-size-4 thin-text">
+        <h3 className="has-text-white-bis is-size-4 thin-text is-size-6-mobile">
           124, Route Cocoterie, Roche Bois
         </h3>
-        <h1 className="has-text-white thin-text title-text">EAT DRINK VISIT</h1>
+        <h1 className="has-text-white thin-text title-text is-size-4-mobile">
+          EAT DRINK VISIT
+        </h1>
       </div>
       <Accueil />
       <Menus />
