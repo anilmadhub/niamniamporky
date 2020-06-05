0513f2fd7cdf08a65e8eeab2b45594ca1d607646
Oumar <oumarkhoda@gmail.com>
2020-06-05 14:29:07 +0400

    galleri and menu

---
+++ components/Menus/MenuDescriptionDesktop.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MenuDescription = ({ title, description }) => {
+  return
+};
+
+export default MenuDescription;
--- pages/galerie.js
+++
@@ -1,9 +0,0 @@
-import Main from "./../components/Layout/Main";
-
-export default function Galerie() {
-  return (
-    <Main>
-      <div>Gallery page</div>
-    </Main>
-  );
-}
