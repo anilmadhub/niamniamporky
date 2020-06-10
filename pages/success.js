088d30d8359c40eb95730f48b6786d7d19dbe113
anilmadhub <a.nilmadhub@gmail.com>
2020-06-10 21:19:22 +0400

    added netlify bot and action

--- pages/success.js
+++ pages/success.js
@@ -1,12 +1,15 @@
 import Main from "./../components/Layout/Main";
 import Contact from "./../components/Contact/Contact";
 
-export default function ContactPage() {
+export default function Success() {
   return (
     <Main>
       <section className="section" id="accueil">
         <div className="container">
           <h1 className="title has-text-centered">Contact & Réservation</h1>
+          <div class="notification is-success">
+            Merci, nous avons bien reçu votre demande de réservation.
+          </div>
           <Contact />
         </div>
       </section>
